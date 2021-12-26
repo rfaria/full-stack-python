2dd5c3f87001a2ee948e460046c75440850926c3
Rodrigo Candido Faria <8796054+rfaria@users.noreply.github.com>
2021-12-26 15:13:19 -0300

    Add Python Crash Course

---
+++ Python Crash Course/hello.py
@@ -0,0 +1,13 @@
+import datetime 
+    
+def main():  
+    # using now() to get current time 
+    current_time = datetime.datetime.now()
+
+    print("Hello, World!")
+    print("Welcome!")
+    print ("Time now is: ", current_time) 
+
+
+if __name__ == '__main__':
+    main()
