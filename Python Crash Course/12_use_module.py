7cccf84640e27506725532920b06928ab16691e9
Rodrigo Candido Faria <8796054+rfaria@users.noreply.github.com>
2021-12-27 14:35:30 -0300

    Organize files' names

---
+++ Python Crash Course/12_use_module.py
@@ -0,0 +1,4 @@
+import new_module as nm
+
+if __name__ == '__main__':
+    nm.say_hi()
---
+++ Python Crash Course/new_module.py
@@ -0,0 +1,11 @@
+def say_hi():
+    print("Hi!")
+
+
+def main():
+    say_hi()
+    say_hi()
+    say_hi()
+
+if __name__ == '__main__':
+    main()
