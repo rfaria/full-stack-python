fe5497107d6f4f3611d1780efea52fe9d5f99b0d
Rodrigo Candido Faria <8796054+rfaria@users.noreply.github.com>
2021-12-28 17:07:52 -0300

    Add functional Django example

--- Django Crash Course/myapp/views.py
+++ Django Crash Course/myapp/views.py
@@ -3,4 +3,18 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Welcome!</h1>')
+    context = {
+        'name': 'John Doe',
+        'age': 45,
+        'nationality': 'British',
+    }
+    return render(request, 'index.html', context)
+
+def greetings(request):
+    name = request.GET['name']
+    return render(request, 'greetings.html', context={'name':name})
+
+def counter(request):
+    text = request.GET['text']
+    words_counter = len(text.split())
+    return render(request, 'counter.html', context={'words_counter':words_counter})
